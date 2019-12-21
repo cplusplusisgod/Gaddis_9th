7e2d1fea1682913fd38913458db28f1f95aa58b2
cplusplusisgod <57809842+cplusplusisgod@users.noreply.github.com>
2019-12-21 13:26:03 -0800

    upload

--- Chapter_9_pointers/ch9_p2_test_scores/ch9_p2_test_scores/ch9_p2_test_scores.cpp
+++ Chapter_9_pointers/ch9_p2_test_scores/ch9_p2_test_scores/ch9_p2_test_scores.cpp
@@ -2,12 +2,69 @@
 //
 
 #include <iostream>
+using namespace std;
+void order(int[], int);
+void average(int[], int);
 
 int main()
 {
-    std::cout << "Hello World!\n";
+	int user_input; 0;
+	int* arr = nullptr;
+
+
+	cout << "Enter size\n";
+	do {
+		cin >> user_input;
+	} while (user_input <= 0);
+	arr = new int[user_input]; //dynamicaly allocate array with user input size;
+
+	for (int count = 0; count < user_input; count++)
+	{
+		cout << "Test score for # " << (count + 1) << "/" << user_input << endl;
+		cin >> arr[count];
+	}
+	for (int count = 0; count < user_input; count++)
+		cout << arr[count] << " ";
+
+	cout << "\n";
+	order(arr,user_input);
+	for (int count = 0; count < user_input; count++)
+		cout << arr[count] << " ";
+
+	average(arr, user_input);
+	delete[] arr;
+
 }
 
+//uses bubble sort
+void order(int arr[], int size)
+{
+
+	int maxElement;
+	int index;
+
+	for (maxElement = size - 1; maxElement > 0; maxElement--)
+	{
+		for (index = 0; index < maxElement; index++)
+		{
+			if (arr[index] > arr[index + 1])
+			{
+				swap(arr[index], arr[index + 1]);
+			}
+		}
+	}
+}
+
+void average(int arr[], int size)
+{
+	int total = 0;
+	int count = 0;
+
+	for (count; count < size; count++)
+		total += arr[count];
+
+	cout << "The total was " << total << " The size was " << size << " Making the average approx " << total / size;
+}
 // Run program: Ctrl + F5 or Debug > Start Without Debugging menu
 // Debug program: F5 or Debug > Start Debugging menu
 
