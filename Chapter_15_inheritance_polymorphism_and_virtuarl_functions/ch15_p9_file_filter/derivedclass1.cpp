ad58836154fc364e5af93c9fcb720ac755e3cb29
rhoona <rhoonabot1@gmail.com>
2020-05-17 21:05:22 -0700

    Encypts and Decrypts file contents.
    This simply encrypts a file's content and then decrpyts it. Does not write to file.

--- Chapter_15_inheritance_polymorphism_and_virtuarl_functions/ch15_p9_file_filter/derivedclass1.cpp
+++ Chapter_15_inheritance_polymorphism_and_virtuarl_functions/ch15_p9_file_filter/derivedclass1.cpp
@@ -1,6 +1,37 @@
 #include "derivedclass1.h"
+#include <fstream>
+#include <iostream>
+#include <vector>
 
-derivedclass1::derivedclass1()
+// THIS CLASS ENCRPYTS AND DECRPYTS A FILE CONTENT. DOES NOT WRITE TO FILE
+derivedclass1::derivedclass1() {}
+
+void
+derivedclass1::alter_file(std::string file) const
 {
 
+  char letter;
+  std::vector<int> lines;
+  std::ifstream readFile;
+
+  // if the file the user wants to open dosnt exsist, exit the functions with
+  // return.
+  readFile.open(file, std::ios::in);
+  if (readFile.fail()) {
+    std::cout << "File " << '"' << " " << file << " " << '"'
+              << " does not exsist.\n";
+    return;
+  }
+
+  // reads the file and changed the letters to int's with -15 value. (encypts)
+  while (readFile.get(letter)) {
+
+    lines.push_back(static_cast<int>(letter - 15));
+  }
+  readFile.close();
+
+  // decrypts and couts the file contents.
+  for (int c : lines) {
+    std::cout << static_cast<char>((c + 15));
+  }
 }
--- Chapter_15_inheritance_polymorphism_and_virtuarl_functions/ch15_p9_file_filter/derivedclass1.h
+++ Chapter_15_inheritance_polymorphism_and_virtuarl_functions/ch15_p9_file_filter/derivedclass1.h
@@ -1,11 +1,12 @@
 #ifndef DERIVEDCLASS1_H
 #define DERIVEDCLASS1_H
+#include "baseclass.h"
 
-
-class derivedclass1
+class derivedclass1 : public Baseclass
 {
 public:
-    derivedclass1();
+  derivedclass1();
+  virtual void alter_file(std::string) const;
 };
 
 #endif // DERIVEDCLASS1_H
