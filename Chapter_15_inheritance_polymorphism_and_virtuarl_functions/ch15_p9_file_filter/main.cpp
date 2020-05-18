03d04a255dc5d4e203f48e4e56afe30dcf06d34c
rhoona <rhoonabot1@gmail.com>
2020-05-17 21:06:55 -0700

    main. contains the virtual function for testing.
    Virtuarl function which works differntly depending which derived function is passed.

--- Chapter_15_inheritance_polymorphism_and_virtuarl_functions/ch15_p9_file_filter/main.cpp
+++ Chapter_15_inheritance_polymorphism_and_virtuarl_functions/ch15_p9_file_filter/main.cpp
@@ -1,9 +1,31 @@
+#include "baseclass.h"
+#include "derivedclass1.h"
+#include "derivedclass2.h"
+#include <fstream>
 #include <iostream>
-
+#include <string>
+#include <vector>
 using namespace std;
+void
+work_on_file(const Baseclass&, const std::string);
 
-int main()
+int
+main()
 {
-    cout << "Hello World!" << endl;
-    return 0;
+  string file = "testfile.txt";
+
+  Baseclass first_class;
+  first_class.alter_file(file);
+  derivedclass1 second_class;
+  // second_class.alter_file(file);
+
+  derivedclass2 third_class;
+  third_class.alter_file(file);
+
+  return 0;
+}
+void
+work_on_file(const Baseclass& object, std::string file)
+{
+  object.alter_file(file);
 }
